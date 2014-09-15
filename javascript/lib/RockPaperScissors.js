94b18b890fb0d4c3d8c1ceff389cd6357a97f4eb
javier <js@silveriofunk.com>
2014-09-15 15:46:51 +0100

    errors fixed

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,9 +1,3 @@
-var PAIRS = {
-  rock:     { beats: 'scissors' },
-  paper:    { beats: 'rock' },
-  scissors: { beats: 'paper' }
-};
-
 function Player(name) {
   this.name = name;
 }
@@ -13,23 +7,29 @@ function Game(player1, player2) {
   this.player2 = player2;
 }
 
+Game.prototype.PAIRS = {
+  rock:     { beats: 'scissors' },
+  paper:    { beats: 'rock' },
+  scissors: { beats: 'paper' }
+};
 
 Player.prototype.picks = function(pick) {
   this.pick = pick;
 };
 
 Game.prototype.winner = function() {
-	if(PAIRS[this.player1.pick]['beats'] == this.player2.pick) {
-    return this.player1;
+	if(this.player1.pick === this.player2.pick)
+  return null;
+
+ 	if(this.PAIRS[this.player1.pick].beats === this.player2.pick) {
+  return this.player1;
 	} else {
 		return this.player2;
 	}
 };
 
 Game.prototype.draw = function() {
-	if(PAIRS[this.player1.pick] == this.player2.pick) {
-		return null
+	if(PAIRS[this.player1.pick]['beats']  == [this.player2.pick]['beats']) {
+		return [null, null, null];
 	}
-};
-
 };
